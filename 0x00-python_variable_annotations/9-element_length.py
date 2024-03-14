2cd18a7e5cbc2cf4623418746ae6440d5a53658b
kibuchijw <kibuchijw@gmail.com>
2024-03-14 14:13:16 +0300

    Fix styling issue

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -8,5 +8,6 @@ from typing import Iterable, Sequence, List, Tuple
 
 def element_length(lst: Iterable[Sequence]
                    ) -> List[Tuple[Sequence, int]]:
-    """Returns a list of tuples containing elements from lst and their respective lengths."""
+    """Returns a list of tuples containing
+    elements from lst and their respective lengths."""
     return [(i, len(i)) for i in lst]
