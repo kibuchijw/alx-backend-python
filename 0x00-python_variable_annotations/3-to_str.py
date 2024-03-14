11b5d5d9ca49f117eee5db23ec260d090f579564
kibuchijw <kibuchijw@gmail.com>
2024-03-14 12:33:19 +0300

    Add a type-annotated function to_str that takes a float n as argument and returns the string representation of the float

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Module that takes a float n as argument
+and returns the string representation of the float.
+"""
+
+
+def to_str(n: float) -> str:
+    """Returns the string representation of a float."""
+    return str(n)
